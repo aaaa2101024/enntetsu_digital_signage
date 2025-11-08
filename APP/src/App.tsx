88b9d2d22d3d8d46320eb01d6c568f0dba8790c7
aaaa1024 <suga.ryota.24@shizuoka.ac.jp>
2025-11-08 17:05:35 +0900

    色々修正

--- APP/src/App.tsx
+++ APP/src/App.tsx
@@ -6,13 +6,14 @@ import type { Busdoko } from "./type/busdoko.type";
 function App() {
 
   const [data, setData] = useState<Busdoko | null>(null);
-  
+  const [count, setCount] = useState<number>(1);
 
 
   // API取得
   useEffect(() => {
     const getData = async () => {
       const url = "http://localhost:8080/get_busdoko";
+      console.error("hoge");
       try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -31,9 +32,22 @@ function App() {
       }
     };
 
-    getData();
-    console.error(data?.delay);
-  })
+    // getData();
+    // setCount(count + 1);
+    // console.error(count);
+    // 2. 1分（60000ミリ秒）ごとにfetchDataを実行するタイマーを設定
+    const intervalId = setInterval(() => {
+      getData();
+      setCount(count + 1);
+      console.error(count);
+    }, 60 * 1000); // 60秒 * 1000ミリ秒 = 1分
+
+    // 3. コンポーネントが非表示（アンマウント）になる際にタイマーを停止する
+    //    これがないと、メモリリークの原因になります。
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
 
   return (
@@ -52,6 +66,7 @@ function App() {
         <div className="touchaku">
           <span></span>
           <span className="delay">遅れ約 {data?.delay} 分</span>
+          <div>ただいま {data?.previous} 個前を走行中...</div>
         </div>
       </div>
     </>
